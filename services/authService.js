26c59d53a63b34434ae535e632fce773f9f2ea6f
Daniel Perez Gonzalez <isc.pgd@gmail.com>
2025-02-12 08:21:26 -0600

    Feat: registrar nuevo usuario en BD

--- services/authService.js
+++ services/authService.js
@@ -1,15 +1,37 @@
 const { response, request } = require('express');
+const bcrypt = require('bcryptjs');
+const User = require('../models/UserModel');
 
-const newUser = (req = request, res = response) => {
-    const { fullName, email, password } = req.body;
+const newUser = async (req = request, res = response) => {
+    const { email, password } = req.body;
+    try {
+        let newUser = await User.findOne({ email });
+        if (!!newUser) {
+            return res.status(400).json({
+                ok: false,
+                message: 'El correo ingresado ya esta registrado'
+            });
+        }
 
-    res.status(201).json({
-        ok: true,
-        msg: 'register',
-        fullName,
-        email,
-        password
-    });
+        newUser = new User(req.body);
+
+        // encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        newUser.password = bcrypt.hashSync(password, salt);
+        // Guardar usuario
+        await newUser.save();
+
+        res.status(201).json({
+            ok: true,
+            uid: newUser.id,
+            fullName: newUser.fullName
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            message: 'Error al guardar en base de datos'
+        });
+    }
 };
 
 const loginUser = (req = request, res = response) => {
